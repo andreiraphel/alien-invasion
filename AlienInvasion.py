fd00987ce52210a3ce61f23f8d8304c497086e91
andreiraphel <andreiraphel@gmail.com>
2022-06-12 10:27:28 +0800

    update

--- AlienInvasion.py
+++ AlienInvasion.py
@@ -52,6 +52,10 @@ class AlienInvasion:
         """Update the position of all aliens in the fleet."""
         self._check_fleet_edges()
         self.aliens.update()
+
+        # Look for alien-ship collisions.
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            print("Ship hit!!!")
         
     def _create_fleet(self):
         """Create the fleet of aliens."""
